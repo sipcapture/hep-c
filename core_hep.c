f8a584c62ef2854a56bc3a86fc8552625031f313
Alexandr Dubovikov <alexandr.dubovikov@gmail.com>
2016-09-13 23:50:01 +0200

    GPL->BSD

--- core_hep.c
+++ core_hep.c
@@ -7,19 +7,21 @@
  *  Author: Alexandr Dubovikov <alexandr.dubovikov@gmail.com>
  *  (C) Homer Project 2012 (http://www.sipcapture.org)
  *
- * Homer capture agent is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version
+ *  Copyright (c) 2010-2016 <Alexandr Dubovikov> 
+ *  All rights reserved.
  *
- * Homer capture agent is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Redistribution and use in source and binary forms are permitted
+ * provided that the above copyright notice and this paragraph are
+ * duplicated in all such forms and that any documentation,
+ * advertising materials, and other materials related to such
+ * distribution and use acknowledge that the software was developed
+ * by the <SIPCAPTURE>. The name of the SIPCAPTURE may not be used to 
+ * endorse or promote products derived from this software without specific 
+ * prior written permission.
+
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
 */
 
